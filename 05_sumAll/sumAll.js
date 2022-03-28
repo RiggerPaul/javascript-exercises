553221a5a32aed111a17f14008c7ccc384b13a49
Paul Alford <paulalford2020@outlook.com>
2022-03-28 12:30:38 +0200

    Exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let c = 0;
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+  if (a < 0 || b < 0) return "ERROR";
+
+  if (a > b) {
+    const d = a;
+    a = b;
+    b = d;
+  }
+  
+  for (i = a; i <= b; i++) {
+    c += i;
+  }
+  return c;
 };
 
 // Do not edit below this line
